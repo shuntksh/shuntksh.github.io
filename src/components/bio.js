2ec120d8426a4af7b8150154e5944c7026f8da95
Shun Takahashi <shun@mbp15.local>
2019-12-04 21:45:39 -0800

    Update gatsby to 2.18.6

---
+++ src/components/bio.js
@@ -0,0 +1,47 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Image from "gatsby-image";
+
+import { rhythm } from "../utils/typography";
+
+const Bio = () => {
+    const data = useStaticQuery(graphql`
+        query BioQuery {
+            site {
+                siteMetadata {
+                    author
+                    social {
+                        twitter
+                    }
+                }
+            }
+        }
+    `);
+
+    const { author, social } = data.site.siteMetadata;
+    return (
+        <div
+            style={{
+                display: "flex",
+                marginBottom: rhythm(2.5),
+            }}
+        >
+            <p>
+                Written by <strong>{author}</strong> who lives and works in San Francisco building
+                useful things.{" "}
+                <a href={`https://twitter.com/${social.twitter}`}>
+                    You should follow him on Twitter
+                </a>
+            </p>
+        </div>
+    );
+};
+
+export default Bio;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,74 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+import React from "react";
+import { Link } from "gatsby";
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import { rhythm, scale } from "../utils/typography";
 
-import Header from "./header"
-import "./layout.css"
+class Layout extends React.Component {
+    render() {
+        const { location, title, children } = this.props;
+        const rootPath = `${__PATH_PREFIX__}/`;
+        let header;
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
+        if (location.pathname === rootPath) {
+            header = (
+                <h1
+                    style={{
+                        ...scale(1.5),
+                        marginBottom: rhythm(1.5),
+                        marginTop: 0,
+                    }}
+                >
+                    <Link
+                        style={{
+                            boxShadow: "none",
+                            textDecoration: "none",
+                            color: "inherit",
+                        }}
+                        to={"/"}
+                    >
+                        {title}
+                    </Link>
+                </h1>
+            );
+        } else {
+            header = (
+                <h3
+                    style={{
+                        fontFamily: "Montserrat, sans-serif",
+                        marginTop: 0,
+                    }}
+                >
+                    <Link
+                        style={{
+                            boxShadow: "none",
+                            textDecoration: "none",
+                            color: "inherit",
+                        }}
+                        to={"/"}
+                    >
+                        {title}
+                    </Link>
+                </h3>
+            );
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+        return (
+            <div
+                style={{
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    maxWidth: rhythm(24),
+                    padding: "${rhythm(1.5)} ${rhythm(3 / 4)}",
+                }}
+            >
+                <header>{header}</header>
+                <main>{children}</main>
+                <footer>
+                    © {new Date().getFullYear()}, Built with{" "}
+                    <a href="https://www.gatsbyjs.org">Gatsby</a>
+                </footer>
+            </div>
+        );
+    }
 }
 
-export default Layout
+export default Layout;
